9ac87ce684709743d189a43935ff829771e2928d
rohithbvs <54908694+rohithbvs@users.noreply.github.com>
2021-09-14 12:51:26 +0530

    Update urls.py

--- FirstDjango/hello/home/urls.py
+++ FirstDjango/hello/home/urls.py
@@ -7,12 +7,10 @@ from . import views
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
-   
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('about/', views.about, name='blog-about'),
     path('like/<int:pk>',views.LikeView,name='like-post'),
     path('post/<int:pk>/comment/', PostCommentView.as_view(), name='add-comment'),
     
